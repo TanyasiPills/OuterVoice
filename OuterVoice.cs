dcb513ab5a7b905c99995a3dc7a450a74472eb1c
Maychii <106334255+TanyasiPills@users.noreply.github.com>
2025-03-09 14:14:37 +0100

    wokrs, oh god, i dont like that

--- OuterVoice.cs
+++ OuterVoice.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Collections;
+using System.Reflection;
 using HarmonyLib;
 using OWML.Common;
 using OWML.ModHelper;
@@ -13,6 +14,8 @@ namespace OuterVoice
 
         private AudioSource _audioSource;
         private AudioClip _customClip;
+
+        private bool playerInGame = false;
 
         public void Awake(){Instance = this;}
 
@@ -32,13 +35,48 @@ namespace OuterVoice
         public void OnCompleteSceneLoad(OWScene previousScene, OWScene newScene)
         {
             if (newScene != OWScene.SolarSystem) return;
+
             ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
-            GameObject player = buddyApi.GetPlayerBody(buddyApi.GetLocalPlayerID());
+            buddyApi.OnPlayerJoin().AddListener(AudioPlayer);
+            
+        }
+
+        private void AudioPlayer(uint playerID)
+        {
+            playerInGame = true;
+            StartCoroutine(WaitForPlayerAndSetupAudio(playerID));
+        }
+
+        private void Update()
+        {
+            if(playerInGame)
+            {
+                if (Input.GetKeyUp(KeyCode.J))
+                {
+                    SpawnAudioPlayer();
+                }
+            }
+        }
+
+        private void SpawnAudioPlayer()
+        {
+
+        }
+
+        private IEnumerator WaitForPlayerAndSetupAudio(uint id)
+        {
+            while (!buddyApi.GetPlayerReady(id))
+            {
+                ModHelper.Console.WriteLine("Waiting for player to be ready...", MessageType.Info);
+                yield return new WaitForSeconds(0.5f);
+            }
+            GameObject player = buddyApi.GetPlayerBody(id);
             if (player == null)
             {
                 ModHelper.Console.WriteLine("Player not found!", MessageType.Error);
-                return;
+                yield break;
             }
+
             _audioSource = player.GetComponent<AudioSource>();
             if (_audioSource == null)
             {
@@ -46,12 +84,26 @@ namespace OuterVoice
             }
 
             _audioSource.playOnAwake = false;
-            _audioSource.loop = false;
+            _audioSource.loop = true;
             _audioSource.spatialBlend = 1f;
             _audioSource.volume = 0.5f;
 
-            StartCoroutine(LoadAudioClip("audio/my_sound.ogg"));
+            LoadAndPlayAudio("Assets/audio/whisle.mp3");
+        }
+
+        private void LoadAndPlayAudio(string path)
+        {
+            _customClip = ModHelper.Assets.GetAudio(path);
+            if (_customClip == null)
+            {
+                ModHelper.Console.WriteLine("Failed to load audio: " + path, MessageType.Error);
+                return;
+            }
+
+            _audioSource.clip = _customClip;
+            _audioSource.Play();
+
+            ModHelper.Console.WriteLine("Playing custom sound!", MessageType.Success);
         }
     }
-
 }
