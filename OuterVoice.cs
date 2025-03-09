6fc7f8bbf4863d9166905250066fe6ae65b92c50
Maychii <106334255+TanyasiPills@users.noreply.github.com>
2025-03-09 18:44:10 +0100

    fixes and stuff

--- OuterVoice.cs
+++ OuterVoice.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using HarmonyLib;
@@ -15,7 +16,6 @@ namespace OuterVoice
         public static IQSBAPI buddyApi;
 
         private AudioClip audioClip;
-        private Camera camera;
 
         private bool playerInGame = false;
 
@@ -28,12 +28,11 @@ namespace OuterVoice
 
         uint myId = 999;
 
-        bool gotId = false;
-
         public void Awake(){Instance = this;}
 
         public void Start()
         {
+            audioSources = new Dictionary<uint, AudioSource>();
             buddyApi = ModHelper.Interaction.TryGetModApi<IQSBAPI>("Raicuparta.QuantumSpaceBuddies");
             ModHelper.Console.WriteLine($"Swompy mompy, {nameof(OuterVoice)} is loaded!", MessageType.Success);
 
@@ -42,10 +41,7 @@ namespace OuterVoice
             OnCompleteSceneLoad(OWScene.TitleScreen, OWScene.TitleScreen);
             LoadManager.OnCompleteSceneLoad += OnCompleteSceneLoad;
 
-            buddyApi.RegisterHandler<AudioClip>("voice", GetVoice);
-
             mic = Microphone.devices[0];
-            StartCoroutine(Record());
         }
 
         private IEnumerator Record()
@@ -55,62 +51,90 @@ namespace OuterVoice
 
             while (Microphone.GetPosition(mic) < 0) yield return null;
 
+            int lastPos = 0;
+
             while (true)
             {
-                yield return new WaitForSeconds(1);
+                yield return new WaitForSeconds(0.008333f);
                 int pos = Microphone.GetPosition(mic);
-                if (pos < freq) continue;
+                if (pos < freq || pos == lastPos) continue;
                 clip.GetData(data, pos - freq);
 
-                AudioClip clipToSend = AudioClip.Create("clipike", data.Length, 1, 44100, false);
-                clipToSend.SetData(data, 0);
-                SendVoice(clipToSend);
+                SendVoice(data);
+
+                lastPos = pos;
             }
 
         }
 
-        private void SendVoice(AudioClip clip)
+        private void SendVoice(float[] data)
         {
-            buddyApi.SendMessage("voice", clip);
-            ModHelper.Console.WriteLine($"Voice sent");
+            if (data != null)
+            {
+                buddyApi.SendMessage("voice", data);
+            }
+            else
+            {
+                ModHelper.Console.WriteLine("Data is null, cannot send voice!", MessageType.Error);
+            }
         }
 
-        private void GetVoice(uint sender, AudioClip clip)
+        private void GetVoice(uint sender, float[] data)
         {
-            audioSources[sender].clip = clip;
-            audioSources[sender].Play();
-            ModHelper.Console.WriteLine($"Playing voice from player {sender}");
+            if (audioSources.ContainsKey(sender))
+            {
+                AudioSource source = audioSources[sender];
+                if (source.clip == null)
+                {
+                    AudioClip clipToPlay = AudioClip.Create("clipike", data.Length, 1, 44100, false);
+                    clipToPlay.SetData(data, 0);
+                    audioSources[sender].clip = clipToPlay;
+                }
+                else
+                {
+                    int currentPos = source.timeSamples;
+                    float[] newClipData = new float[source.clip.samples + data.Length];
+                    source.clip.GetData(newClipData, 0);
+                    Array.Copy(data, 0, newClipData, currentPos, data.Length);
+                    source.clip.SetData(newClipData, 0);
+                }
+                if (!source.isPlaying)
+                {
+                    source.Play();
+                }
+            }
+            else
+            {
+                ModHelper.Console.WriteLine($"Audio source for player {sender} is not initialized!", MessageType.Error);
+            }
+
         }
 
         public void OnCompleteSceneLoad(OWScene previousScene, OWScene newScene)
         {
             if (newScene != OWScene.SolarSystem) return;
 
-            camera = Camera.main;
-
             ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
             buddyApi.OnPlayerJoin().AddListener(PlayerJoined);
-            
+
+            myId = buddyApi.GetLocalPlayerID();
+
+            ModHelper.Console.WriteLine($"yam", MessageType.Success);
+            uint[] ids = buddyApi.GetPlayerIDs();
+            foreach (uint id in ids)
+            {
+                if (id != myId) StartCoroutine(WaitForPlayerAndSetupAudio(id));
+            }
+            ModHelper.Console.WriteLine($"joined as: {myId}");
+            buddyApi.GetPlayerReady(myId);
+            StartCoroutine(Record());
+            buddyApi.RegisterHandler<float[]>("voice", GetVoice);
         }
 
         private void PlayerJoined(uint playerID)
         {
-            if (buddyApi.GetLocalPlayerID() == playerID)
-            {
-                myId = playerID;
-                gotId = true;
-                uint[] ids = buddyApi.GetPlayerIDs();
-                foreach (uint id in ids)
-                {
-                    if(id != playerID) StartCoroutine(WaitForPlayerAndSetupAudio(playerID));
-                }
-            }
-            else
-            {
-                StartCoroutine(WaitForPlayerAndSetupAudio(playerID));
-            }
+            StartCoroutine(WaitForPlayerAndSetupAudio(playerID));
             ModHelper.Console.WriteLine($"Player id: {playerID}");
-
         }
         
         private IEnumerator WaitForPlayerAndSetupAudio(uint id)
@@ -131,6 +155,11 @@ namespace OuterVoice
             if (souce == null)
             {
                 souce = player.AddComponent<AudioSource>();
+            }
+
+            if (audioSources == null)
+            {
+                audioSources = new Dictionary<uint, AudioSource>();
             }
 
             souce.playOnAwake = false;
