5733294221948de56e1fa99059aba87c59d0d499
Maychii <106334255+TanyasiPills@users.noreply.github.com>
2025-03-09 13:47:04 +0100

    got QSBAPI in project

--- OuterVoice.cs
+++ OuterVoice.cs
@@ -2,23 +2,23 @@
 using HarmonyLib;
 using OWML.Common;
 using OWML.ModHelper;
+using UnityEngine;
 
 namespace OuterVoice
 {
     public class OuterVoice : ModBehaviour
     {
         public static OuterVoice Instance;
+        public static IQSBAPI buddyApi;
 
-        public void Awake()
-        {
-            Instance = this;
-            // You won't be able to access OWML's mod helper in Awake.
-            // So you probably don't want to do anything here.
-            // Use Start() instead.
-        }
+        private AudioSource _audioSource;
+        private AudioClip _customClip;
+
+        public void Awake(){Instance = this;}
 
         public void Start()
         {
+            buddyApi = ModHelper.Interaction.TryGetModApi<IQSBAPI>("Raicuparta.QuantumSpaceBuddies");
             // Starting here, you'll have access to OWML's mod helper.
             ModHelper.Console.WriteLine($"My mod {nameof(OuterVoice)} is loaded!", MessageType.Success);
 
@@ -33,6 +33,24 @@ namespace OuterVoice
         {
             if (newScene != OWScene.SolarSystem) return;
             ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+            GameObject player = buddyApi.GetPlayerBody(buddyApi.GetLocalPlayerID());
+            if (player == null)
+            {
+                ModHelper.Console.WriteLine("Player not found!", MessageType.Error);
+                return;
+            }
+            _audioSource = player.GetComponent<AudioSource>();
+            if (_audioSource == null)
+            {
+                _audioSource = player.AddComponent<AudioSource>();
+            }
+
+            _audioSource.playOnAwake = false;
+            _audioSource.loop = false;
+            _audioSource.spatialBlend = 1f;
+            _audioSource.volume = 0.5f;
+
+            StartCoroutine(LoadAudioClip("audio/my_sound.ogg"));
         }
     }
 
